ff1eb9e3a7b5038929bd98e2c000196c01c66341
Camila Reinke <reinkecamila@gmail.com>
2026-03-21 16:48:21 -0300

    build: estrutura do projeto

---
+++ src/test/java/com/bn/projects/ProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
