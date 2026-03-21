34aa2dff9d6b908d18f20fb5dd9ea5df95496cd8
Camila Reinke <reinkecamila@gmail.com>
2026-03-21 17:03:37 -0300

    feat: cria camada de serviços ProjetoService

---
+++ src/main/java/com/bn/projects/services/ProjetoService.java
@@ -0,0 +1,33 @@
+package com.bn.projects.services;
+
+import com.bn.projects.models.ProjetoModel;
+import com.bn.projects.repositories.ProjetoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProjetoService {
+
+    @Autowired
+    private ProjetoRepository projetoRepository;
+
+    public ProjetoModel criarProjeto(ProjetoModel projetoModel) {
+        return projetoRepository.save(projetoModel);
+    }
+
+    public List<ProjetoModel> buscarTodosProjetos() {
+        return projetoRepository.findAll();
+    }
+    
+    public Optional<ProjetoModel> buscarPorId(Long id) {
+        return projetoRepository.findById(id);
+    }
+
+    public void deletarProjeto(Long id) {
+        projetoRepository.deleteById(id);
+    }
+
+}
