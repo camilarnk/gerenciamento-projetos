4394a9e521dd1ed385ebfa9693aa777070070b57
Camila Reinke <reinkecamila@gmail.com>
2026-03-21 16:59:46 -0300

    feat: cria entidade Projeto

---
+++ src/main/java/com/bn/projects/models/ProjetoModel.java
@@ -0,0 +1,53 @@
+package com.bn.projects.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_projeto")
+public class ProjetoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_projeto")
+    private String nome;
+
+    @Column(name = "data_inicio_projeto")
+    private LocalDate dataInicio;
+
+    @Column(name = "data_termino_projeto")
+    private LocalDate dataFim;
+
+    public ProjetoModel() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+}
